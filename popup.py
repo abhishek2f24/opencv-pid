7108ab3fd202c859defd0ba5b1942157637bc5da
Abhishek Kumar Maurya <abhishek2f24@gmail.com>
2023-07-11 18:01:33 +0530

    Update popup.py

--- popup.py
+++ popup.py
@@ -43,3 +43,33 @@ x1, y1, x3, y3 = 100, 100, 200, 200
 popup_text = 'This is a popup annotation.'
 
 add_popup_annotation(input_pdf_path, output_pdf_path, x1, y1, x3, y3, popup_text)
+
+
+import fitz
+
+def add_popup_annotation(input_pdf_path, output_pdf_path, x1, y1, x3, y3, popup_text):
+    doc = fitz.open(input_pdf_path)
+    page = doc[0]  # Assuming you want to add the popup to the first page
+
+    # Create the rectangle annotation
+    rectangle = fitz.Rect(x1, y1, x3, y3)
+    rectangle_annot = page.add_rect_annot(rectangle)
+
+    # Create the popup annotation
+    popup_annot = page.add_freetext_annot(rectangle.tl, popup_text)
+    popup_annot.set_flag(fitz.ANNOT_FLAG_POPUP)
+
+    # Associate the popup annotation with the rectangle annotation
+    rectangle_annot.set_popup(popup_annot)
+
+    doc.save(output_pdf_path, incremental=True)
+    doc.close()
+
+# Usage example
+input_pdf_path = 'input.pdf'
+output_pdf_path = 'output.pdf'
+x1, y1, x3, y3 = 100, 100, 200, 200
+popup_text = 'This is a popup annotation.'
+
+add_popup_annotation(input_pdf_path, output_pdf_path, x1, y1, x3, y3, popup_text)
+
